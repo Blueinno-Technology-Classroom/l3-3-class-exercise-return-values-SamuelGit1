3d26773d3a2b8854174cb199c3011c7aed4d5230
Samuel <wongsamuel12321@gmail.com>
2024-02-03 14:57:38 +0800

    submit

--- main.py
+++ main.py
@@ -3,27 +3,68 @@
 Q1: 
 '''
 
-# TODO: Write your code here
+def cube_a_number(x):
+    result = x * x * x
+    return result
+
+cubed_res = cube_a_number(10)
+print(f'The result is {cubed_res}.')
 
 ##################################################
 '''
 Q2:
 '''
 
-# TODO: Write your code here
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+ 
+result = is_even(13)    # result => True or False
+print(f'13 is even: {result}')
 
 ##################################################
 '''
 Q3:
 '''
 
-# TODO: Write your code here
+def is_even(n):
+    return n % 2 == 0
+ 
+result = is_even(13)    # result => True or False
+print(f'13 is even: {result}')
 
 ##################################################
 '''
-Challenge:
+Q4:
 '''
 
-# TODO: Write your code here
+def score_to_gpa(score):
+    return round(score / 25, 1)
+
+alice_score = score_to_gpa(50)  # 2.0
+bob_score = score_to_gpa(90)    # 3.6
+
+##################################################
+'''
+Challenge
+'''
+
+def middle_of_3(n1, n2, n3):
+   if n1 < n2:
+        if n2 < n3:
+           return n2
+        elif n1 < n3:
+           return n3
+        else:
+            return n1
+   else:
+        if n3 < n2:
+            return n2
+        elif n1 < n3:
+            return n1
+        else:
+            return n3
 
 ##################################################
